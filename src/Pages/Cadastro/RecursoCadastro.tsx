39307976c67d4dd183ed9974261d8ee697decd6e
gucordeiro26 <gucordeiro26@gmail.com>
2024-11-21 21:14:23 -0300

    configurando fetch operações

--- src/Pages/Cadastro/RecursoCadastro.tsx
+++ src/Pages/Cadastro/RecursoCadastro.tsx
@@ -53,7 +53,7 @@ export const RecursoCadastro: FC<RecursoProps> = ({ title }) => {
             }
 
             // Fetch para o backend
-            const response = await fetch('/api/recursos', {
+            const response = await fetch('http://127.0.0.1:8000/api/operacoes', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
